2ae974387777215d2796d3e5db61007daea18019
Ghada NOUAIRIA <nouairiaghada@gmail.com>
2024-07-27 16:00:26 +0200

    Step 4 of the pipeline, mandatory
    This is the main result of the pipeline, finding Differentially methylated positions

---
+++ find_dmp_4.R
@@ -0,0 +1,60 @@
+#! usr/R
+##This script reads M-values and Beta values of methylation arrays and computes DMPs, Differentially methylated positions
+##Input = beta values of pre-processed methylation data 
+##output = csv files with p-values for the DMPs in each comparison of categories
+
+##Computations of DMPs and DMRs using ChAMP and minfi
+
+library(dplyr)
+library("readr")
+library("minfi")
+library(tibble)
+library("ChAMP")
+library(doParallel)
+library(rio)
+
+###input=RData objects for PSC+CCA
+###output= DMRs and DMPs for PSC compared with PSC+CCA
+
+###Parallel computation
+registerDoParallel(makePSOCKcluster(3))
+
+
+##Load previously saved data (RData objects, for more details, please look at pre-processing.Rmd, cell_composition_correction.R and rem_conf_probes_adj_age.R
+bVals <- read_csv("results/bVals.csv")
+metadata <- read_csv("results/preprocess_metadata.csv") %>%
+            filter(sample_id %in% c(colnames(bVals)))
+####Match metadata order to columns in bVals
+metadata <- metadata[match(c(colnames(bVals)), metadata$sample_id),]
+
+##Choose the samples field
+##Choosing only the field with information of the category of each samples (can be multiple)
+Samples <- metadata$sample_id
+Class <- metadata$group
+
+##Choose the adjustment method: can be "BH", 
+Method = "BH"
+
+##Choose array type: ("EPIC" or "450K")
+ARRAY = "EPIC"
+
+###Choose adjusted P value
+P = 0.05
+
+###Computing DMPs
+dmp_data <- champ.DMP(beta = bVals[,Samples], 
+          pheno = Class, 
+          adjPVal = P, 
+          adjust.method = Method, 
+          arraytype = ARRAY) 
+
+export_list(x, file = "results/dmp_champ.%s.csv")
+
+###Finding DMPs with another method (required binary categories of "Class", it will work with multiple categories but the results do not specify which comparison it is)
+dmp_minfi <- dmpFinder(bVals[,Samples], 
+                 Class, 
+          type = "categorical") %>% 
+          filter(pval < P) %>%
+          arrange(pval) 
+
+write_csv(dmp_minfi, file = "results/dmp_minfi.csv")
